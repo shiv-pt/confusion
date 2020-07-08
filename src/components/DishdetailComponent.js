797bf7c8d624ab14f7f256c49c07d543de11253a
shiv-pt <shivamp.is19@rvce.edu.in>
2020-07-08 13:02:19 +0530

    Redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, BreadcrumbItem, Breadcrumb, CardImg, CardBody, CardText, CardTitl
 Input, FormGroup, Label, Modal,ModalHeader,ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form'
+import { Loading } from './loadingComponent'
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -60,6 +61,24 @@ function RenderComments({comments, addComment, dishId}) {
     }
 
     const DishDetail = (props) => {
+        if(props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
         const dish = props.dish
         if (dish == null) {
             return (<div></div>)
